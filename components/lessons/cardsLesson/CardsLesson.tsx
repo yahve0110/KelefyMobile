0d5b45d157aa049216c3181cb1ae88b7ca3fa4c1
Ilya Skorokhodov <ilik124darc@gmail.com>
2024-11-25 14:01:09 +0200

    cards

--- components/lessons/cardsLesson/CardsLesson.tsx
+++ components/lessons/cardsLesson/CardsLesson.tsx
@@ -1,36 +1,254 @@
 import Colors from "@/constants/Colors";
-import React from "react";
-import { Text, View,StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withTiming,
+} from "react-native-reanimated";
+import { FontAwesome } from "@expo/vector-icons";
+import InExNetsBtn from "@/components/ExerciseNavihationBtn/ExerciseNavihationBtn";
+import { Audio } from "expo-av";
+import ExerciseNavihationBtn from "@/components/ExerciseNavihationBtn/ExerciseNavihationBtn";
 
 interface Card {
-    en: string;
-    et: string;
-    image_url: string;
-    audio_url: string;
-    description: string;
-  }
+  en: string;
+  et: string;
+  image_url: string;
+  audio_url: string;
+  description: string;
+}
 
 interface Props {
   data: Card[];
-
 }
 
-const CardsLesson: React.FC<Props> = (props:Props) => {
-    const {data} = props
+const CardsLesson: React.FC<Props> = (props: Props) => {
+  const { data } = props;
+  const [flipped, setFlipped] = useState(false);
+  const rotation = useSharedValue(0);
+  const [sound, setSound] = useState<Audio.Sound | null>(null);
+
+  const [currentCard, setCurrentCard] = useState(0);
+
+  const nextCard = () => {
+    setCurrentCard(currentCard + 1);
+  };
+
+  const prevCard = () => {
+    setCurrentCard(currentCard - 1);
+  };
+
+  const flipCard = () => {
+    if (flipped) {
+      rotation.value = withTiming(0, { duration: 500 });
+    } else {
+      rotation.value = withTiming(180, { duration: 500 });
+    }
+    setFlipped(!flipped);
+  };
+
+  const frontAnimatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotateY: `${rotation.value}deg` }],
+    opacity: rotation.value < 90 ? 1 : 0,
+  }));
+
+  const backAnimatedStyle = useAnimatedStyle(() => ({
+    transform: [{ rotateY: `${rotation.value - 180}deg` }],
+    opacity: rotation.value >= 90 ? 1 : 0,
+  }));
+
+  const handleSoundPress = async () => {
+    try {
+      const { sound: newSound } = await Audio.Sound.createAsync({
+        uri: data[currentCard].audio_url,
+      });
+      await newSound.playAsync();
+
+      if (sound) {
+        await sound.unloadAsync();
+      }
+      setSound(newSound);
+    } catch (error) {
+      console.error("Ошибка воспроизведения:", error);
+    }
+  };
+
+  React.useEffect(() => {
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+  }, [sound]);
+
   return (
-    <View>
-      <Text>{data[0].description}</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Click the card to see the translation</Text>
+      <TouchableWithoutFeedback onPress={flipCard}>
+        <View style={styles.cardsContainer}>
+          {/* Front Side */}
+          <Animated.View style={[styles.card, frontAnimatedStyle]}>
+            <Image
+              source={{ uri: data[currentCard].image_url }}
+              style={styles.image}
+              onError={(error) => console.error("Error loading image:", error)}
+            />
+            <Text style={styles.word}>{data[currentCard].et}</Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.starButton}
+                onPress={(e) => {
+                  e.preventDefault();
+                  // логика для звездочки
+                }}
+              >
+                <FontAwesome name="star-o" size={38} style={styles.starIcon} />
+              </TouchableOpacity>
+            </View>
+          </Animated.View>
+
+          {/* Back Side */}
+          <Animated.View
+            style={[styles.card, styles.backCard, backAnimatedStyle]}
+          >
+            <Text style={styles.word}>{data[currentCard].en}</Text>
+            <View style={styles.divider}></View>
+            <Text style={styles.description}>
+              {data[currentCard].description}
+            </Text>
+          </Animated.View>
+        </View>
+      </TouchableWithoutFeedback>
+      <View style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.soundButton} onPress={handleSoundPress}>
+          <FontAwesome name="volume-up" size={32} color={Colors.light.text} />
+        </TouchableOpacity>
+        {currentCard > 0 && (
+          <ExerciseNavihationBtn onPress={prevCard} text="Prev" />
+        )}
+        <ExerciseNavihationBtn onPress={nextCard} text="Next" />
+      </View>
     </View>
   );
 };
 
 export default CardsLesson;
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: Colors.light.background,
+  container: {
+    flex: 1,
+    backgroundColor: Colors.light.background,
+    padding: 20,
+  },
+  text: {
+    fontSize: 18,
+    color: Colors.light.text,
+    textAlign: "center",
+    marginBottom: 20,
+    opacity: 0.8,
+  },
+  cardsContainer: {
+    width: "90%",
+    height: "78%",
+    alignSelf: "center",
+    perspective: "1000px",
+  },
+  card: {
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: Colors.light.cardsBackground,
+    backfaceVisibility: "hidden",
+    borderRadius: 20,
+    borderWidth: 2,
+    borderColor: "gray",
+    padding: 20,
+  },
+  backCard: {
+    backgroundColor: Colors.light.cardsBackground,
+    transform: [{ rotateY: "180deg" }],
+    justifyContent: "center",
+  },
+  image: {
+    width: 220,
+    aspectRatio: 1,
+    resizeMode: "contain",
+    borderRadius: 10,
+  },
+  word: {
+    fontSize: 34,
+    color: Colors.light.text,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  divider: {
+    width: "60%",
+    height: 2,
+    backgroundColor: Colors.light.itemsColor,
+    marginVertical: 10,
+  },
+  description: {
+    fontSize: 18,
+    textAlign: "center",
+    paddingHorizontal: 10,
+    color: Colors.light.itemsColor,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  starIcon: {
+    color: Colors.light.gold,
+  },
+  starButton: {
+    padding: 10,
+    borderRadius: 10,
+  },
+  soundButton: {
+    padding: 10,
+    borderRadius: 10,
+    aspectRatio: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.light.itemsColor,
+  },
+  nextButton: {
+    alignSelf: "flex-end",
+    padding: 15,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    backgroundColor: Colors.light.itemsColor,
+    marginTop: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-  });
-  
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  nextText: {
+    color: Colors.light.text,
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  bottomContainer: {
+    marginTop: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
--- data.ts
+++ data.ts
@@ -38,12 +38,57 @@ export const data: LessonsData = {
       type: "cards",
       data: [
         {
-          en: "Hello!",
-          et: "Tere!",
-          image_url: "https://firebasestorage.googleapis.com/...",
-          audio_url: "https://www.book2.nl/book2/ET/SOUND/0043.mp3",
-          description: "A common informal greeting."
-        }
+            en: "Hello!",
+            et: "Tere!",
+            image_url:
+              "https://firebasestorage.googleapis.com/v0/b/keelefy.appspot.com/o/lesson1%2Fhello_1.png?alt=media&token=78bed978-afe7-4c69-9816-00bd71141f16",
+            audio_url: "https://www.book2.nl/book2/ET/SOUND/0043.mp3",
+            description: "A common informal greeting.",
+          },
+          {
+            en: "Good day!",
+            et: "Tere päevast!",
+            description: "A more formal greeting used during the day.",
+            audio_url: "https://www.book2.nl/book2/ET/SOUND/0044.mp3",
+            image_url:
+              "https://firebasestorage.googleapis.com/v0/b/keelefy.appspot.com/o/lesson1%2Fhello_2.png?alt=media&token=7a28a257-8d06-4888-8ba9-f076c920d16c",
+          },
+          {
+            en: "Good morning!",
+            et: "Tere hommikust!",
+            description: "A morning greeting.",
+            audio_url:
+              "https://firebasestorage.googleapis.com/v0/b/keelefy.appspot.com/o/lesson1%2FTere%20hommikust!.mp3?alt=media&token=9f73128f-5552-4117-bba1-64e88c083a3e",
+            image_url:
+              "https://firebasestorage.googleapis.com/v0/b/keelefy.appspot.com/o/lesson1%2Fmorning.png?alt=media&token=19595786-ead5-4479-9154-de8da1e5142d",
+          },
+          {
+            en: "How are you?",
+            et: "Kuidas läheb?",
+            audio_url: "https://www.book2.nl/book2/ET/SOUND/0045.mp3",
+            image_url:
+              "https://firebasestorage.googleapis.com/v0/b/keelefy.appspot.com/o/lesson1%2FhowIsGoing.jpeg?alt=media&token=d2fd54bd-512a-46a3-bd7b-dfe65387c0b7",
+            description:
+              "A question about the well-being of the person being addressed.",
+          },
+          {
+            en: "Good!",
+            et: "Hästi!",
+            audio_url:
+              "https://firebasestorage.googleapis.com/v0/b/keelefy.appspot.com/o/lesson1%2Fh%C3%A4sti%20.mp3?alt=media&token=f43375ae-3581-42bf-a089-a4ed68155ec7",
+            image_url:
+              "https://firebasestorage.googleapis.com/v0/b/keelefy.appspot.com/o/lesson1%2Fgood.jpeg?alt=media&token=c7cd5196-a73f-4e1c-9941-6d28d23a38b0",
+            description: "A response to the question about one's well-being.",
+          },
+          {
+            en: "Good evening!",
+            et: "Tere õhtust!",
+            audio_url:
+              "https://firebasestorage.googleapis.com/v0/b/keelefy.appspot.com/o/lesson1%2FTere%20%C3%B5htust!.mp3?alt=media&token=8c325087-82cb-4d53-9f10-6b433a0c8793",
+            image_url:
+              "https://firebasestorage.googleapis.com/v0/b/keelefy.appspot.com/o/lesson1%2Fevening.png?alt=media&token=55b07c69-705e-480a-ab1d-dd898cb8fa45",
+            description: "A greeting used in the evening.",
+          },
       ]
     },
   },
